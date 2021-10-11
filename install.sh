49a176cd8b002c77e52680d9e595a5e6eb9a11d6
Joao Jacome <github@joaojaco.me>
2021-10-12 00:54:50 +0100

    dunst

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ rm -rf $HOME/.config/polybar
 rm -rf $HOME/.config/config
 rm -rf $HOME/.config/rofi
 rm -rf $HOME/.config/alacritty
-#rm -rf $HOME/.config/termite
+rm -rf $HOME/.config/dunst
 ln -s $(pwd)/i3 $HOME/.config/i3
 ln -s $(pwd)/polybar $HOME/.config/polybar
 ln -s $(pwd)/rofi $HOME/.config/rofi
-#ln -s $(pwd)/termite $HOME/.config/termite
 ln -s $(pwd)/picom $HOME/.config/picom
 ln -s $(pwd)/alacritty $HOME/.config/alacritty
+ln -s $(pwd)/dunst $HOME/.config/dunst
 
 echo "source $HOME/dotfiles/.profile-extra" >> $HOME/.profile
 echo "source $HOME/dotfiles/zshrc.sh" >> $HOME/.zshrc
